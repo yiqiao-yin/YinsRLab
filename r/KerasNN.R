9822f36ca097b1e19f7b669e4f42ca39ea9aa608
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2021-04-28 18:02:13 -0400

    Update KerasNN.R

--- r/KerasNN.R
+++ r/KerasNN.R
@@ -10,87 +10,134 @@ KerasNN <- function(
   x = x,
   y = y,
   cutoff = .9,
-  validation_split = 0.1,
+  validation_split = 1 - cutoff,
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(),
   batch_size = 128,
+  numberOfLayers = 1,
   activation = 'relu',
-  l1.units = 256,
-  l2.units = 128,
-  l3.units = 64,
+  useBias = FALSE,
+  finalactivation = 'softmax',
+  l1.units = 128,
+  l2.units = 64,
   epochs = 30
 ) {
 
   # Package
-  library(keras); library(knitr)
+  library(keras)
 
   # Data
-  all <- data.frame(cbind(y,x))
+  all <- data.frame(cbind(y, x))
 
   # Setup
-  x_train <- as.matrix(all[1:round(cutoff*nrow(all),0), -1])
-  y_train <- as.matrix(all[1:round(cutoff*nrow(all),0), 1])
-  x_test <- as.matrix(all[(round(cutoff*nrow(all),0)+1):nrow(all), -1])
-  y_test <- as.matrix(all[(round(cutoff*nrow(all),0)+1):nrow(all), 1])
-  # dim(x_train); dim(y_train); dim(x_test); dim(y_test)
+  train_idx <- 1:round(cutoff*nrow(all),0)
+  x_train <- as.matrix(all[train_idx, -1])
+  y_train <- as.matrix(all[train_idx, 1])
+  x_test <- as.matrix(all[-train_idx, -1])
+  y_test <- as.matrix(all[-train_idx, 1])
 
   # Check levels for response
   number.of.levels <- nrow(plyr::count(y_train))
+  num_classes <- number.of.levels
 
   # To prepare this data for training we one-hot encode the
   # vectors into binary class matrices using the Keras to_categorical() function
   y_train <- to_categorical(y_train, number.of.levels)
   y_test <- to_categorical(y_test, number.of.levels)
-  # dim(x_train); dim(y_train); dim(x_test); dim(y_test)
 
   # Defining the Model
-  model <- keras_model_sequential()
-  model %>%
-    layer_dense(units = l1.units, activation = activation, input_shape = c(ncol(x_train))) %>%
-    layer_dropout(rate = 0.4) %>%
-    layer_dense(units = l2.units, activation = activation) %>%
-    layer_dropout(rate = 0.2) %>%
-    layer_dense(units = l3.units, activation = activation) %>%
-    layer_dropout(rate = 0.2) %>%
-    layer_dense(units = number.of.levels, activation = 'softmax')
-  summary(model)
+  if (numberOfLayers == 1) {
+    model <- keras_model_sequential()
+    model %>%
+      layer_dense(
+        units = number.of.levels,
+        input_shape = c(ncol(x_train)),
+        activation = finalactivation,
+        use_bias = useBias)
+    summary(model)
+  } else if (numberOfLayers == 2) {
+    model <- keras_model_sequential()
+    model %>%
+      layer_dense(units = l1.units, activation = activation, use_bias = useBias, input_shape = c(ncol(x_train))) %>%
+      layer_dense(units = number.of.levels, use_bias = useBias, activation = finalactivation)
+    summary(model)
+  } else if (numberOfLayers == 3) {
+    model <- keras_model_sequential()
+    model %>%
+      layer_dense(units = l1.units, activation = activation, use_bias = useBias, input_shape = c(ncol(x_train))) %>%
+      layer_dense(units = l2.units, activation = activation, use_bias = useBias) %>%
+      layer_dense(units = number.of.levels, use_bias = useBias, activation = finalactivation)
+    summary(model)
+  } else {
+    print("============== WARNING ==============")
+    print("Input value for [numberOfLayers] must be 1, 2, or 3.")
+    print("Since none of the values above are entered, the default is set to 1.")
+    print("=====================================")
+    model <- keras_model_sequential()
+    model %>%
+      layer_dense(
+        units = number.of.levels,
+        input_shape = c(ncol(x_train)),
+        activation = finalactivation,
+        use_bias = useBias)
+    summary(model)
+  }
 
   # Next, compile the model with appropriate loss function, optimizer, and metrics:
   model %>% compile(
-    loss = 'categorical_crossentropy',
-    optimizer = optimizer_rmsprop(),
-    metrics = c('accuracy')
-  )
+    loss = loss,
+    optimizer = optimizer,
+    metrics = c('accuracy') )
 
   # Training and Evaluation
   history <- model %>% fit(
     x_train, y_train,
     epochs = epochs,
     batch_size = batch_size,
-    validation_split = cutoff
+    validation_split = validation_split
   ); plot(history)
 
   # Evaluate the model's performance on the test data:
-  # model %>% evaluate(x_test, y_test)
+  scores = model %>% evaluate(x_test, y_test)
 
   # Generate predictions on new data:
   y_test_hat <- model %>% predict_classes(x_test)
-  y_test <- as.matrix(all[(round(cutoff*nrow(all),0)+1):nrow(all), 1])
+  y_test_hat_raw <- model %>% predict_proba(x_test); colnames(y_test_hat_raw) = c(0:(num_classes-1))
+  y_test <- as.matrix(all[-train_idx, 1])
+  y_test <- as.numeric(as.character(y_test))
   confusion.matrix <- table(Y_Hat = y_test_hat, Y = y_test)
   test.acc <- sum(diag(confusion.matrix))/sum(confusion.matrix)
   all.error <- plyr::count(y_test - cbind(y_test_hat))
+  y_test_eval_matrix <- cbind(
+    y_test=y_test,
+    y_test_hat=y_test_hat,
+    y_test_hat_raw=y_test_hat_raw )
+
+  # AUC/ROC
+  if ((num_classes == 2) && (nrow(plyr::count(y_test_hat)) == 2)) {
+    y_test_for_roc = c(y_test)
+    y_test_class1_for_roc = c(y_test_hat_raw[, 2])
+    AUC_test <- pROC::roc(response = y_test_for_roc, predictor = y_test_class1_for_roc)
+  } else {
+    print("Estimate do not have enough levels.")
+    AUC_test <- 0.5
+  }
 
   # Return
   return(
     list(
+      Model = list(model = model, scores = scores),
       x_train = x_train,
       y_train = y_train,
       x_test = x_test,
       y_test = y_test,
       y_test_hat = y_test_hat,
+      y_test_eval_matrix = y_test_eval_matrix,
       Training.Plot = plot(history),
-      Confusion.Matrix = confusion.matrix,
-      Confusion.Matrix.Pretty = knitr::kable(confusion.matrix),
+      Confusion.Matrix = list(regularTable = confusion.matrix, prettyTable = knitr::kable(confusion.matrix)),
       Testing.Accuracy = test.acc,
-      All.Types.of.Error = all.error
+      All.Types.of.Error = all.error,
+      Test_AUC = AUC_test
     )
   )
 } # End of function
